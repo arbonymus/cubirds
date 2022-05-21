d858d38d013bff9672a5277a7c941c0891c2f499
arbonymus <104995887+arbonymus@users.noreply.github.com>
2022-05-21 10:34:58 +0200

    Update Carta.java

--- Carta.java
+++ Carta.java
@@ -9,12 +9,12 @@ public class Carta {
     private String nombre;
     private int numbandpeq;
     private int numbandgrande;
-    private int numCartas;
-    public Carta(String nombre, int numbandpeq, int numbandgrande, int numCartas) {
+    
+    public Carta(String nombre, int numbandpeq, int numbandgrande) {
         this.nombre = nombre;
         this.numbandpeq = numbandpeq;
         this.numbandgrande = numbandgrande;
-        this.numCartas=numCartas;
+        
     }
 
     public String getNombre() {
@@ -29,42 +29,15 @@ public class Carta {
         return numbandgrande;
     }
 
-    public int getNumCartas() {
-        return numCartas;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setNumbandpeq(int numbandpeq) {
-        this.numbandpeq = numbandpeq;
-    }
-
-    public void setNumbandgrande(int numbandgrande) {
-        this.numbandgrande = numbandgrande;
-    }
-
-    public void setNumCartas(int numCartas) {
-        this.numCartas = numCartas;
-    }
-    
-    
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         
         sb.append(nombre);
         sb.append(" (").append(numbandpeq);
-        sb.append("/").append(numbandgrande);
-        sb.append("): ").append(numCartas).append(" cartas");
+        sb.append("/").append(numbandgrande).append(")");
        
         return sb.toString();
     }
-
-    
-    
-    
-
 }
+
